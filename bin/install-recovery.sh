32f5514d100804fd3afd95cc36fac7d49e6557d2
Shekhawat2 <anandsingh215@yahoo.com>
2019-01-14 22:05:55 +0530

    miui_HMNote6ProGlobal_V10.2.1.0.OEKMIXM_8f244ad2cd_8.1.zip

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:32441678:9427da49957f80666adc2af1ae7c7b015ac786f7; then
-  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:26781002:bc306d3293c79e259135465985317aaf313b96cd EMMC:/dev/block/bootdevice/by-name/recovery 9427da49957f80666adc2af1ae7c7b015ac786f7 32441678 bc306d3293c79e259135465985317aaf313b96cd:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:32441678:788083b329ca6f2f896b844bcdb3995b921cb371; then
+  applypatch -b /system/etc/recovery-resource.dat EMMC:/dev/block/bootdevice/by-name/boot:26781002:90bfb9d6cb0ea04f986bfb64e9e80b88b78203b5 EMMC:/dev/block/bootdevice/by-name/recovery 788083b329ca6f2f896b844bcdb3995b921cb371 32441678 90bfb9d6cb0ea04f986bfb64e9e80b88b78203b5:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
